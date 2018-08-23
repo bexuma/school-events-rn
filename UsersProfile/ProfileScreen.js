aa082c5e47036258d54b606b22b44a70a8237c02
Bexultan Myrzatay <bexultan.dev@gmail.com>
2018-08-24 02:21:00 +0600

    1.0.1 update

--- UsersProfile/ProfileScreen.js
+++ UsersProfile/ProfileScreen.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import { Text, View, Button, TouchableOpacity, StyleSheet, ScrollView, Image, AsyncStorage } from 'react-native';
+import { Text, View, Button, Dimensions, TouchableOpacity, FlatList, ActivityIndicator, StyleSheet, ScrollView, Image, AsyncStorage } from 'react-native';
 import FollowButton from './FollowButton';
 import Navigator from './Navigator';
-import {
-  SimpleLineIcons
-} from '@expo/vector-icons';
+import { SimpleLineIcons } from '@expo/vector-icons';
 import { graphql } from 'react-apollo';
 import { gql } from 'apollo-boost';
+import Moment from 'moment';
 
 const findProfileQuery = gql`
   query ($userId: ID!) {
@@ -16,7 +15,18 @@ const findProfileQuery = gql`
       username
       avatar
       participatingEvents {
-        title
+        id
+        image_name
+        title 
+        starts_at
+        prices {
+          amount
+        }
+        hostedBy {
+          id
+          username
+          avatar
+        }
       }
     }
   }
@@ -77,6 +87,96 @@ class ProfileScreen extends Component {
     }
   }
 
+  renderPrices = prices => {
+    let text;
+    if (prices.length === 1) {
+      text = `\u20B8${prices[0].amount}`;
+    } else if (prices.length > 1) {
+      const amounts = prices.map(price => price.amount);
+      amounts.sort((a, b) => a - b);
+      text = `\u20B8${amounts[0]} - \u20B8${amounts[amounts.length - 1]}`;
+    } else {
+      text = 'Свободный вход';
+    }
+
+    return <Text>{text}</Text>;
+  };
+
+  renderEvent = ({ item }) => {
+    const { navigation } = this.props;
+    const screenWidth = Dimensions.get('window').width;
+    
+    const Host = (
+          <TouchableOpacity onPress={() => {navigation.navigate('Profile', {
+            userId: item.hostedBy.id,
+            username: item.hostedBy.username
+          })}}>
+              <Text style={{ fontWeight: 'bold' }}>
+                {item.hostedBy.username}
+              </Text>
+          </TouchableOpacity>
+    );
+
+    const Title = <Text style={{ fontSize: 16 }}>{item.title}</Text>;
+
+    const Datetime = (
+      <Text style={{ color: 'grey', fontSize: 14 }}>
+        {Moment(item.starts_at).format('Do MMMM, HH:mm')}
+      </Text>
+    );
+
+    return (
+      <TouchableOpacity
+        activeOpacity={1}
+        style={styles.item}
+        onPress={() => {
+          navigation.navigate('Event', {
+            event: item,
+            user: this.state.user,
+          });
+        }}>
+        <Image
+          style={{
+            height: Math.round((screenWidth * 9) / 16),
+            width: screenWidth,
+          }}
+          source={{
+            uri: item.imageUrl,
+          }}
+        />
+        <View style={styles.info}>
+          <TouchableOpacity style={styles.profilePic} onPress={() => {navigation.navigate('Profile', {
+            userId: item.hostedBy.id,
+            username: item.hostedBy.username
+          })}}>
+            <Image
+              style={{ height: screenWidth / 7.5, width: screenWidth / 7.5 }}
+              borderRadius={screenWidth / 15}
+              source={{
+                uri: item.avatarUrl,
+              }}
+            />
+          </TouchableOpacity>
+          <View style={styles.text}>
+            {Title}
+            {Host}
+            {Datetime}
+            {this.renderPrices(item.prices)}
+          </View>
+          <TouchableOpacity style={styles.kebab}>
+            <SimpleLineIcons name="options-vertical" size={20} color="#26A4FF" />
+          </TouchableOpacity>
+        </View>
+        {/* <View style={{ padding: 16, paddingTop: 0, paddingBottom: 24}}>
+          <Text>
+            идут <Text style={{ fontWeight: 'bold' }}>assankhanov, zhakulin99</Text> и
+            <Text style={{ fontWeight: 'bold' }}> еще 28</Text>
+          </Text>
+        </View> */}
+      </TouchableOpacity>
+    );
+  };
+
   goToHistory = () => {
     this.setState(previousState => {
       return { history: true };
@@ -122,10 +222,15 @@ class ProfileScreen extends Component {
 
   render() {
     if (this.state.isLoading) {
-      return <View></View>
+      return (
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
     }
 
     const user = this.props.data.findUser
+    const participatingEvents = user.participatingEvents
 
     const User_events = (
       <TouchableOpacity>
@@ -166,6 +271,22 @@ class ProfileScreen extends Component {
       </View>
     );
 
+    const History = props => {
+      return (
+        <FlatList
+          data={props.participatingEvents}
+          renderItem={this.renderEvent}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      )
+      
+    }
+
+    const Plans = props => {
+      console.log(props)
+      return <View></View>
+    }
+
     return (
       <ScrollView style={styles.container}>
         <View style={styles.image}>
@@ -190,12 +311,12 @@ class ProfileScreen extends Component {
           </View>
           <FollowButton />
         </View>
-          <Navigator goToHistory={this.goToHistory} goToPlans={this.goToPlans}/>
-          <View style={{flex: 1}}>
-          <Text>
-          {this.state.history ? 'History' : 'Plans'}
-          </Text>
-          </View>
+
+        <Navigator goToHistory={this.goToHistory} goToPlans={this.goToPlans}/>
+
+        <View style={{flex: 1}}>
+          {this.state.history ? <History participatingEvents={participatingEvents} /> : <Plans participatingEvents={participatingEvents}/>}
+        </View>
       </ScrollView>
     );
   }
@@ -247,6 +368,34 @@ const styles = StyleSheet.create({
   nouns: {
     color: '#86CCFF',
     fontSize: 12,
+  },
+  item: {
+    backgroundColor: 'white',
+    flex: 1,
+    borderBottomColor: '#D5D5D5',
+    borderBottomWidth: 1,
+  },
+  info: {
+    padding: 16,
+    flex: 1,
+    flexDirection: 'row',
+    paddingTop: 8,
+    paddingBottom: 24,
+  },
+  profilePic: {
+    flex: 3,
+  },
+  text: {
+    flex: 14,
+    paddingLeft: 16,
+  },
+  kebab: {
+    flex: 1.5,
+    alignItems: 'flex-end',
+  },
+  whoIsIn: {
+    flex: 5,
+    justifyContent: 'center',
   },
 });
 
