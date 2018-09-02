6d9d4bfa81de06da6e3718c74bff668ef4a8f421
Bexultan Myrzatay <bexultan.dev@gmail.com>
2018-09-02 18:29:42 +0600

    minor refactoring

--- utils/textHelper.js
+++ utils/textHelper.js
@@ -2,15 +2,13 @@ import Moment from 'moment';
 
 const TextHelper = {
   getFormattedPrice: (prices) => {
-    let text;
+    let text = 'Свободный вход';
     if (prices.length === 1) {
       text = `\u20B8${prices[0].amount}`;
     } else if (prices.length > 1) {
       const amounts = prices.map(price => price.amount);
       amounts.sort((a, b) => a - b);
       text = `\u20B8${amounts[0]} - \u20B8${amounts[amounts.length - 1]}`;
-    } else {
-      text = 'Свободный вход';
     }
 
     return text
